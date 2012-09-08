358d9c8c0918669391371fc7c0801d14b8869906
Kasper Johansen <k@spernj.org>
2012-09-08 10:32:50 +0200

    Various fixes.

--- lib/gtk2_window_settings.rb
+++ lib/gtk2_window_settings.rb
@@ -67,16 +67,18 @@ class Gtk2_window_settings
   def on_window_size_request(*args)
     Gtk.timeout_remove(@size_request_timeout) if @size_request_timeout
     
-    @size_request_timeout = Gtk.timeout_add(500) do
-      @size_request_timeout = nil
-      
-      if @window and !@window.destroyed?
-        size = @window.size
-        @db.update(:Gtk2_window_settings, {:width => size[0], :height => size[1]}, {:id => @id}) if size[0].to_i > 0 and size[1].to_i > 0
+    @size_request_timeout = Gtk.timeout_add(500, &lambda{
+      begin
+        @size_request_timeout = nil
+        
+        if @window and !@window.destroyed?
+          size = @window.size
+          @db.update(:Gtk2_window_settings, {:width => size[0], :height => size[1]}, {:id => @id}) if size[0].to_i > 0 and size[1].to_i > 0
+        end
+      ensure
+        return false
       end
-      
-      false
-    end
+    })
     
     #If false isnt returned the event might be canceled, which can lead to very buggy behaviour.
     return false
